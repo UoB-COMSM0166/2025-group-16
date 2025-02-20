b937430501221a59644c76b66060be0a43268463
chi <chiderlin36@gmail.com>
2025-02-20 21:23:44 +0000

    fix: merge error

--- docs/src/pages/WelcomeIntro.js
+++ docs/src/pages/WelcomeIntro.js
@@ -1,47 +1,58 @@
 class WelcomeIntro extends BasePage {
   constructor() {
     super();
-    this.maskIsVisible = false; 
-    this.maskColor = color(211, 211, 211, 150); 
-    this.boxWidth = width-4; 
-    this.boxHeight = 200; 
+    this.maskIsVisible = false;
+    this.maskColor = color(211, 211, 211, 150);
+    this.boxWidth = width - 4;
+    this.boxHeight = 200;
     this.boxX = (width - this.boxWidth) / 2;
-    this.boxY = height - this.boxHeight-4; 
+    this.boxY = height - this.boxHeight - 4;
   }
 
   showWelcomeIntro() {
-    this.maskIsVisible = true; 
+    this.maskIsVisible = true;
   }
 
-  hideWelcomeIntro(){
+  hideWelcomeIntro() {
     this.maskIsVisible = false;
   }
 
-  draw(){
-    if (this.maskIsVisible){
+  draw() {
+    if (this.maskIsVisible) {
       //Grey transparent background
-      push(); 
+      push();
       fill(this.maskColor);
-      rect(0, 0, width, height); 
-      pop(); 
-    
+      rect(0, 0, width, height);
+      pop();
+
       //Robot
       const resources =
         Resources.images.entity.PLAYER[Theme.palette.player.red].IDLE.DOWN[0];
-      const scale = Constants.EntitySize.XL; 
+      const scale = Settings.entity.scale[Constants.EntitySize.XL];
       const scaledWidth = resources.width * scale;
       const scaledHeight = resources.height * scale;
-      image(resources.image, this.boxX + 30, this.boxY - 200, scaledWidth, scaledHeight);
+      image(
+        resources.image,
+        this.boxX + 150,
+        this.boxY - 100,
+        scaledWidth,
+        scaledHeight,
+      );
 
       //BoxShadow
       push();
       stroke(Theme.palette.black);
-      strokeWeight(3);  // 設置合適的線條寬度
+      strokeWeight(3); // 設置合適的線條寬度
       drawingContext.shadowColor = color(0, 0, 0, 255); // 設置陰影顏色
-      drawingContext.shadowBlur = 15;  // 設置陰影模糊程度，不要太大
+      drawingContext.shadowBlur = 15; // 設置陰影模糊程度，不要太大
       drawingContext.shadowOffsetX = 0; // 設置陰影水平偏移量
       drawingContext.shadowOffsetY = 5; // 設置陰影垂直偏移量
-      line(this.boxX, this.boxY + this.boxHeight, this.boxX + this.boxWidth, this.boxY + this.boxHeight); // 繪製底部邊框
+      line(
+        this.boxX,
+        this.boxY + this.boxHeight,
+        this.boxX + this.boxWidth,
+        this.boxY + this.boxHeight,
+      ); // 繪製底部邊框
       pop();
 
       //Box
@@ -57,8 +68,16 @@ class WelcomeIntro extends BasePage {
       fill(Theme.palette.black);
       textSize(Theme.text.fontSize.medium);
       textAlign(LEFT, TOP);
-      text('Welcome to UNSTOPPABLE. Player 1: move with [W A S D], ', 10, height-175);
-      text('Player 2: move with [↑ ← ↓ →] to COME HERE area to start! Shall we?', 10, height-125);
+      text(
+        'Welcome to UNSTOPPABLE. Player 1: move with [W A S D], ',
+        10,
+        height - 175,
+      );
+      text(
+        'Player 2: move with [↑ ← ↓ →] to COME HERE area to start! Shall we?',
+        10,
+        height - 125,
+      );
       pop();
 
       //Press
@@ -66,16 +85,14 @@ class WelcomeIntro extends BasePage {
       fill(Theme.palette.black);
       textSize(Theme.text.fontSize.medium);
       textAlign(RIGHT, BOTTOM);
-      text('Press →', width-10, height-10);
+      text('Press →', width - 10, height - 10);
       pop();
     }
   }
-  
-  keyPressed(){
-    if (this.maskIsVisible && keyCode === RIGHT_ARROW){
+
+  keyPressed() {
+    if (this.maskIsVisible && keyCode === RIGHT_ARROW) {
       this.hideWelcomeIntro();
     }
   }
-
-
 }
