e3077afb0d6a61bfe268fd3b2e26183b733e4674
ChenPin-Ru <nezii0331@gmail.com>
2025-03-01 18:28:55 +0000

    Fix: Map intro keep pressing any keys will cause flash

--- docs/src/pages/maps/MapIntro1.js
+++ docs/src/pages/maps/MapIntro1.js
@@ -11,8 +11,11 @@ class MapIntro1 extends BaseMapIntro {
       ],
     });
 
+    this.countdown = 5;
+    this.isCountingDown = false;
     this.background = Resources.images.map.game1;
     this.playerRobot = null;
+    this.playerAvatar = null;
     this.enemyRobots = [];
     // For button "Press →"
     this.pressButtonArea = {
@@ -40,6 +43,25 @@ class MapIntro1 extends BaseMapIntro {
       },
     });
 
+    // // Create a player avatar to demonstrate attacks
+    // this.playerAvatar = new Player({
+    //   idx: 0,
+    //   controls: {},
+    //   shapeType: Constants.EntityType.PLAYER,
+    //   size: Constants.EntitySize.M,
+    //   color: Theme.palette.player.red,
+    //   position: { x: width / 3, y: height / 2 },
+    // });
+
+    // Create a single robot for the demo
+    this.demoRobot = new Robot({
+      idx: 0,
+      x: width / 2,
+      y: height / 2 - 50,
+      size: Constants.EntitySize.M,
+      isMovable: false,
+    });
+
     // Create robot for demo
     const startX = width / 2;
     const startY = height / 2 - 100;
@@ -60,6 +82,7 @@ class MapIntro1 extends BaseMapIntro {
         );
       }
     }
+    // this.startDemoAttack();
   }
 
   /** @override */
@@ -72,11 +95,15 @@ class MapIntro1 extends BaseMapIntro {
       background(Theme.palette.lightGrey);
     }
 
+    // Draw the player avatar for demo
+    if (this.playerAvatar) {
+      this.playerAvatar.draw();
+    }
+
     // Draw all enemy robots
     this.enemyRobots.forEach((robot) => {
       push();
       translate(robot.x, robot.y);
-      scale(1.2);
       translate(-robot.x, -robot.y);
       robot.draw();
       pop();
@@ -135,7 +162,79 @@ class MapIntro1 extends BaseMapIntro {
     textAlign(RIGHT, CENTER);
     text('Press →', width - 25, height - 25);
     pop();
+
+    // add progress box
+    if (this.isCountingDown && this.countdown > 0) {
+      let progress = (5 - this.countdown) / 5;
+
+      let barWidth = 300;
+      let barHeight = 30;
+      let x = width - barWidth - 20;
+      let y = 20;
+
+      // progress outside box
+      push();
+      stroke(255);
+      strokeWeight(4);
+      noFill();
+      rect(x, y, barWidth, barHeight);
+      pop();
+
+      // Progress box
+      push();
+      noStroke();
+      fill(50, 50, 50);
+      rect(x, y, barWidth * progress, barHeight);
+      pop();
+    }
   }
+
+  // startDemoAttack() {
+  //   // Flag to track which robot we're attacking
+  //   let currentRobotIndex = 0;
+
+  //   this.attackInterval = setInterval(() => {
+  //     if (currentRobotIndex >= this.enemyRobots.length || this.demoCompleted) {
+  //       clearInterval(this.attackInterval);
+  //       this.demoCompleted = true;
+  //       return;
+  //     }
+
+  //     let targetRobot = this.enemyRobots[currentRobotIndex];
+
+  //     // Move avatar near the robot
+  //     this.playerAvatar.position = {
+  //       x: targetRobot.x - 50,
+  //       y: targetRobot.y,
+  //     };
+
+  //     // Add a visual attack indicator
+  //     setTimeout(() => {
+  //       // Simulate attack animation
+  //       this.playerAvatar.attack();
+
+  //       // Make robot show hit effect
+  //       setTimeout(() => {
+  //         targetRobot.status = Constants.EntityStatus.HIT;
+
+  //         // After a moment, move to next robot
+  //         setTimeout(() => {
+  //           currentRobotIndex++;
+
+  //           // If we've attacked two robots, end the demo
+  //           if (currentRobotIndex >= 2) {
+  //             this.demoCompleted = true;
+  //             clearInterval(this.attackInterval);
+  //             // Start countdown after showing demo
+  //             setTimeout(() => {
+  //               this.startCountdown();
+  //             }, 1000);
+  //           }
+  //         }, 500);
+  //       }, 200);
+  //     }, 300);
+  //   }, 2000); // Attack every 2 seconds
+  // }
 
   /** @override */
   mousePressed() {
@@ -148,13 +247,66 @@ class MapIntro1 extends BaseMapIntro {
       mouseY >= this.pressButtonArea.y &&
       mouseY <= this.pressButtonArea.y + this.pressButtonArea.height
     ) {
-      Controller.changePage(new MapGame1(), Constants.Page.MAP_GAME_1);
+      this.startCountdown();
+      // startCountdown(new MapGame1(), Constants.Page.MAP_GAME_1);
+      // Controller.changePage(new MapGame1(), Constants.Page.MAP_GAME_1);
     }
   }
+
+  // /** @override */
+  // mousePressed() {
+  //   super.mousePressed();
+  //   // Check if click is within the press button area
+  //   if (
+  //     mouseX >= this.pressButtonArea.x &&
+  //     mouseX <= this.pressButtonArea.x + this.pressButtonArea.width &&
+  //     mouseY >= this.pressButtonArea.y &&
+  //     mouseY <= this.pressButtonArea.y + this.pressButtonArea.height
+  //   ) {
+  //     // Skip the demo if it's still running and start countdown
+  //     if (!this.demoCompleted) {
+  //       this.demoCompleted = true;
+  //       clearInterval(this.attackInterval);
+  //     }
+  //     this.startCountdown();
+  //   }
+  // }
 
   /** @override */
   keyPressed() {
     super.keyPressed();
-    Controller.changePage(new MapGame1(), Constants.Page.MAP_GAME_1);
+    this.startCountdown();
+    // Controller.changePage(new MapGame1(), Constants.Page.MAP_GAME_1);
+  }
+
+  // /** @override */
+  // keyPressed() {
+  //   super.keyPressed();
+  //   // Skip the demo if it's still running and start countdown
+  //   if (!this.demoCompleted) {
+  //     this.demoCompleted = true;
+  //     clearInterval(this.attackInterval);
+  //   }
+  //   this.startCountdown();
+  // }
+
+  startCountdown() {
+    // Only start countdown if demo is completed or skipped
+    // if (!this.demoCompleted) {
+    //   this.demoCompleted = true;
+    //   clearInterval(this.attackInterval);
+    // }
+
+    // Don't start countdown if it's already counting down
+    if (this.isCountingDown) return;
+    this.isCountingDown = true;
+    let interval = setInterval(() => {
+      if (this.countdown > 1) {
+        this.countdown--;
+      } else {
+        clearInterval(interval);
+        Controller.changePage(new MapGame1(), Constants.Page.MAP_GAME_1);
+      }
+    }, 1000);
   }
 }
