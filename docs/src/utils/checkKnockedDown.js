9d45e442278257e0f3c668e7c47830183a82ba14
ViviLin <vivi2393142@gmail.com>
2025-03-12 00:30:00 +0000

    feat(checkKnockedDown): check base on fist position

--- docs/src/utils/checkKnockedDown.js
+++ docs/src/utils/checkKnockedDown.js
@@ -1,3 +1,17 @@
+const fistPosition = {
+  [Constants.EntityMove.LEFT]: { x: -9, y: 8.5, w: 6, h: 8 },
+  [Constants.EntityMove.RIGHT]: { x: 9, y: 8.5, w: 6, h: 8 },
+  [Constants.EntityMove.UP]: { x: 9, y: 8, w: 7.5, h: 8 },
+  [Constants.EntityMove.DOWN]: { x: 9, y: 8, w: 7.5, h: 8 },
+};
+
+const originSize = {
+  [Constants.EntityMove.LEFT]: { w: 24, h: 32 },
+  [Constants.EntityMove.RIGHT]: { w: 24, h: 32 },
+  [Constants.EntityMove.UP]: { w: 26, h: 32 },
+  [Constants.EntityMove.DOWN]: { w: 26, h: 32 },
+};
+
 /**
  * Checks if the target entity is knocked down by the hitter entity based on the hit direction.
  *
@@ -6,49 +20,39 @@
  * @returns {boolean} - Returns true if the target entity is knocked down, otherwise false.
  */
 function checkKnockedDown(hitterEntity, targetEntity) {
-  const hitter = {
-    x: hitterEntity.x,
-    y: hitterEntity.y,
-    w: hitterEntity.getShape().scaledWidth,
-    h: hitterEntity.getShape().scaledHeight,
-  };
+  const hitterFist = getFistPosition(hitterEntity); // 使用新函數取得拳頭位置
   const target = {
     x: targetEntity.x,
     y: targetEntity.y,
     w: targetEntity.getShape().scaledWidth,
     h: targetEntity.getShape().scaledHeight,
   };
-  const hitDirection = hitterEntity.direction;
-  switch (hitDirection) {
-    case Constants.EntityMove.LEFT:
-      return (
-        hitter.x > target.x &&
-        hitter.x < target.x + target.w &&
-        hitter.y + hitter.h / 2 < target.y + target.h &&
-        hitter.y + hitter.h / 2 > target.y
-      );
-    case Constants.EntityMove.RIGHT:
-      return (
-        hitter.x < target.x &&
-        hitter.x + hitter.w > target.x &&
-        hitter.y + hitter.h / 2 < target.y + target.h &&
-        hitter.y + hitter.h / 2 > target.y
-      );
-    case Constants.EntityMove.UP:
-      return (
-        hitter.y > target.y &&
-        hitter.y < target.y + target.h &&
-        hitter.x + hitter.w / 2 < target.x + target.w &&
-        hitter.x + hitter.w / 2 > target.x
-      );
-    case Constants.EntityMove.DOWN:
-      return (
-        hitter.y < target.y &&
-        hitter.y + hitter.h > target.y &&
-        hitter.x + hitter.w / 2 < target.x + target.w &&
-        hitter.x + hitter.w / 2 > target.x
-      );
-    default:
-      return false;
-  }
+
+  return (
+    hitterFist.x + hitterFist.w / 2 > target.x - target.w / 2 &&
+    hitterFist.x - hitterFist.w / 2 < target.x + target.w / 2 &&
+    hitterFist.y + hitterFist.h / 2 > target.y - target.w / 2 &&
+    hitterFist.y - hitterFist.h / 2 < target.y + target.h / 2
+  );
+}
+
+/**
+ * Calculates the fist position of the hitter entity based on its direction and scale.
+ *
+ * @param {Entity} hitterEntity - The entity that is hitting.
+ * @returns {Object} - The fist's position and size { x, y, w, h }.
+ */
+function getFistPosition(entity) {
+  const hitter = {
+    w: entity.getShape().scaledWidth,
+    h: entity.getShape().scaledHeight,
+  };
+  const scale = hitter.w / originSize[entity.direction].w;
+
+  return {
+    x: entity.x + fistPosition[entity.direction].x * scale,
+    y: entity.y + fistPosition[entity.direction].y * scale,
+    w: fistPosition[entity.direction].w * scale,
+    h: fistPosition[entity.direction].h * scale,
+  };
 }
